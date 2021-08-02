727795a4eef7019e33c17118fe3bad9bc4c1bbd7
km.kim <rhkdck1@gmail.com>
2021-08-02 18:31:32 +0900

    첫 커밋

---
+++ Assets/Scripts/PlayerSettings.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EControlType
+{
+    Mouse,
+    KeyboardMouse
+}
+
+public class PlayerSettings : MonoBehaviour
+{
+    public static EControlType controlType;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
